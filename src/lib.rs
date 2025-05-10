a2dcaf90628403f089a12588eb1e0b830c802d19
Data <the_alchemist@fastmail.com>
2025-05-10 11:05:29 -0400

    sync

--- src/lib.rs
+++ src/lib.rs
@@ -49,33 +49,41 @@ pub enum ComputationDevice {
 /// Call this in the `main()` fn of `build.rs`. `cuda_files`'s first parameter
 /// must be the entry point. The other parameters are just so the build step
 /// knows when to re-compile.
-pub fn build(architecture: GpuArchitecture, cuda_filers: &[&str]) {
-    if cuda_filers.len() < 1 {
+pub fn build(architecture: GpuArchitecture, cuda_files: &[&str]) {
+    if cuda_files.len() < 1 {
         return;
     }
 
     // Tell Cargo that if the given file changes, to rerun this build script.
-    for kernel in cuda_filers {
+    for kernel in cuda_files {
         println!("cargo:rerun-if-changed={kernel}");
     }
 
     let compilation_result = Command::new("nvcc")
         .args([
-            cuda_filers[0],
+            cuda_files[0],
             "-gencode",
             &architecture.gencode_val(),
             "-ptx",
             "-O3", // optimized/release mode.
         ])
-        .output()
-        .expect("Problem compiling the CUDA module.");
+        .output();
 
-    if !compilation_result.status.success() {
-        panic!(
-            "CUDA compilation problem:\nstatus: {}\nstdout: {}\nstderr: {}",
-            compilation_result.status,
-            String::from_utf8_lossy(&compilation_result.stdout),
-            String::from_utf8_lossy(&compilation_result.stderr)
-        );
+    match compilation_result {
+        Ok(output) => {
+            if output.status.success() {
+                println!("Compiled the following CUDA files: {cuda_files:?}");
+            } else {
+                eprintln!(
+                    "CUDA compilation problem:\nstatus: {}\nstdout: {}\nstderr: {}",
+                    output.status,
+                    String::from_utf8_lossy(&output.stdout),
+                    String::from_utf8_lossy(&output.stderr)
+                );
+            }
+        }
+        Err(e) => eprintln!("Unable to compile CUDA files: {e}")
     }
+
+
 }
